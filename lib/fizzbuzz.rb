0551a12801b648d6f9ef9251cfa2c3b2c565b058
ajbater <amanda22@hotmail.co.uk>
2016-11-10 09:55:17 +0000

    tests for divisibility by 3, 5 and 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def divide_by_3(number)
   number % 3 == 0
 end
+
+def divide_by_5(number)
+  number % 5 == 0
+end
+
+def divide_by_3_and_5(number)
+  number % 5 == 0 && number % 3 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,24 @@ describe 'fizzbuzz' do
     expect(divide_by_3(3)).to eq true
   end
 
+  it 'tests for indivisibility by 3' do
+    expect(divide_by_3(4)).to eq false
+  end
+
+  it 'tests for divisibility by 5' do
+    expect(divide_by_5(5)).to eq true
+  end
+
+  it 'tests for indivisibility by 5' do
+    expect(divide_by_5(6)).to eq false
+  end
+
+  it 'tests for divisibilty by 3 and 5' do
+    expect(divide_by_3_and_5(15)).to eq true
+  end
+
+  it 'tests for indivisibility by 3 and 5' do
+    expect(divide_by_3_and_5(16)).to eq false
+  end
+
 end
